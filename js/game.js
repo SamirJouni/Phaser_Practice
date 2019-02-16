c499a9cc0cd667147db0c5fd7d1fb822c3010091
F1r3byte <f1r3byte@yandex.com>
2019-02-16 23:23:05 +0100

    added update function

--- js/game.js
+++ js/game.js
@@ -25,22 +25,41 @@ gameScene.create = function () {
 
 	// player
 
-	const player = this.add.sprite(70, gameH/2, 'player');
+	this.player = this.add.sprite(70, gameH/2, 'player');
 	// player.depth = 1;
 
-	player.setScale(.8);
+	this.player.setScale(.5);
 
 	// enemy
 
-	const enemy = this.add.sprite(250, gameH/2, 'dragon');
-	enemy.scaleX = 2;
-	enemy.scaleY = 2;
+	this.enemy = this.add.sprite(250, gameH/2, 'dragon');
+	// this.enemy.scaleX = 2;
+	// this.enemy.scaleY = 2;
+	// this.enemy.flipX = true;
+
+	// this.enemy.angle = 45;
+	// this.enemy.setAngle(-45);
+	// this.enemy.setOrigin(0,0);
+	// this.enemy.rotation = Math.PI / 4;
+	// this.enemy.setRotation(Math.PI / 4);
 
 	// enemy2
 
-	const enemy2 = this.add.sprite(450, gameH/2, 'dragon');
+	// const enemy2 = this.add.sprite(450, gameH/2, 'dragon');
+	// enemy2.displayWidth = 100;
+	// enemy2.flipY = true;
 
 }
+
+// Update
+ gameScene.update = function () {
+	 this.enemy.angle += 1;
+
+	 if (this.player.scaleX < 2 ) {
+		 this.player.scaleX += 0.01;
+		 this.player.scaleY += 0.01;
+	 }
+ };
 
 // Config
 

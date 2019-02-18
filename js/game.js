01cb82236f150a0290e9ef959e7228e0cd7c74a0
F1r3byte <f1r3byte@yandex.com>
2019-02-17 12:47:50 -1200

    added treasure

--- js/game.js
+++ js/game.js
@@ -1,28 +1,28 @@
 // Scene
 
-const gameScene  = new Phaser.Scene('Game');
+const gameScene = new Phaser.Scene("Game");
 
 // Initialization
 
-gameScene.init = function () {
+gameScene.init = function() {
 	this.playerSpeed = 1.5;
 	this.enemySpeed = 2;
 	this.enemyMaxY = 280;
-	this.enemyMinY= 80;
-}
+	this.enemyMinY = 80;
+};
 
 // Preload
 
-gameScene.preload = function () {
-	this.load.image('background', 'assets/background.png');
-	this.load.image('player', 'assets/player.png');
-	this.load.image('dragon', 'assets/dragon.png');
-	this.load.image('treasure', 'assets/treasure.png');
-}
+gameScene.preload = function() {
+	this.load.image("background", "assets/background.png");
+	this.load.image("player", "assets/player.png");
+	this.load.image("dragon", "assets/dragon.png");
+	this.load.image("treasure", "assets/treasure.png");
+};
 
 // Create
 
-gameScene.create = function () {
+gameScene.create = function() {
 
 	// dimensions
 
@@ -31,26 +31,31 @@ gameScene.create = function () {
 
 	// background
 
-	const bg = this.add.sprite(0, 0, 'background');
-	bg.setPosition(gameW/2, gameH/2);
+	const bg = this.add.sprite(0, 0, "background");
+	bg.setPosition(gameW / 2, gameH / 2);
 
 	// player
 
-	this.player = this.add.sprite(40, gameH/2, 'player');
-	this.player.setScale(.5);
+	this.player = this.add.sprite(40, gameH / 2, "player");
+	this.player.setScale(0.5);
+
+	// Treasure
+
+	this.treasure = this.add.sprite(gameW - 80, gameH / 2, "treasure");
+	this.treasure.setScale(0.6);
 
 	// dragon
 
-	this.dragon = this.add.sprite(250, gameH/2, 'dragon');
-
-}
+	this.dragon = this.add.sprite(250, gameH / 2, "dragon");
+};
 
 // Update
- gameScene.update = function () {
-	 if(this.input.activePointer.isDown){
-		 this.player.x += this.playerSpeed;
-	 }
- };
+
+gameScene.update = function() {
+	if (this.input.activePointer.isDown) {
+		this.player.x += this.playerSpeed;
+	}
+};
 
 // Config
 
@@ -59,7 +64,7 @@ const config = {
 	width: 640,
 	height: 360,
 	scene: gameScene
-}
+};
 
 // Game
 
